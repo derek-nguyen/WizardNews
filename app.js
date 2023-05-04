ae9a1506b798f05c5c79a5339e983bd39606b470
Derek Nguyen <derek.nguyen02@gmail.com>
2023-05-03 20:35:59 -0700

    added routes and some styling

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
 const express = require("express");
+const morgan = require('morgan');
+const path = require('path')
+const { list, find } = require('./postBank');
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+
+app.get("/posts", (req, res) => {
+  const posts = list();
+  // res.send(posts.map(post => post.id))
+  res.send(
+    `<!DOCTYPE html>
+    <html>
+      <head>
+        <title>Wizard News</title>
+        <link rel="stylesheet" href="/style.css" />
+      </head>
+      <body>
+        <div class="news-list">
+          <header><img src="/logo.png"/>Wizard News</header>
+          ${posts.map(post => `
+            <div class='news-item'>
+              <p>
+                <span class="news-position">${post.id}. ▲</span>
+                ${post.title}
+                <small>(by ${post.name})</small>
+              </p>
+              <small class="news-info">
+                ${post.upvotes} upvotes | ${post.date}
+              </small>
+            </div>`
+      ).join('')}
+        </div>
+      </body>
+    </html>`
+  )
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 1337;
 
