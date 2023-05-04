d9d1e36ae97816fc662f625b1c28d83a7ae97122
Derek Nguyen <derek.nguyen02@gmail.com>
2023-05-03 21:07:19 -0700

    finished project

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const morgan = require('morgan');
 const path = require('path')
-const { list, find } = require('./postBank');
+const postBank = require("./postBank");
 const app = express();
 
 app.use(morgan('dev'));
 
 app.get("/posts", (req, res) => {
-  const posts = list();
+  const posts = postBank.list();
   // res.send(posts.map(post => post.id))
   res.send(
     `<!DOCTYPE html>
@@ -23,23 +23,57 @@ app.get("/posts", (req, res) => {
             <div class='news-item'>
               <p>
                 <span class="news-position">${post.id}. ▲</span>
-                ${post.title}
+                <a href="/posts/${post.id}">${post.title}</a>
                 <small>(by ${post.name})</small>
               </p>
               <small class="news-info">
                 ${post.upvotes} upvotes | ${post.date}
               </small>
             </div>`
-      ).join('')}
+    ).join('')}
         </div>
       </body>
     </html>`
   )
 });
 
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+
+  if (!post.id) {
+    throw new Error('Not Found')
+  }
+
+  res.send(
+    `<!DOCTYPE html>
+    <html>
+      <head>
+        <title>Wizard News</title>
+        <link rel="stylesheet" href="/style.css" />
+      </head>
+      <body>
+        <div class="news-list">
+          <header><img src="/logo.png"/>Wizard News</header>
+            <div class='news-item'>
+              <p>
+                <span class="news-position">${post.id}. ▲</span>
+                ${post.title}
+                <small>(by ${post.name})</small>
+              </p>
+              <small class="news-info">
+                ${post.upvotes} upvotes | ${post.date}
+              </small>
+            </div>
+        </div>
+      </body>
+    </html>`
+  )
+})
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = 1337;
+const { PORT = 1337 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
